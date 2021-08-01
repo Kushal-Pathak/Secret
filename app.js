38bc6c888fdb02326bd957eb22570cf650ff0188
Kushal Pathak <kushalpathak80@gmail.com>
2021-08-01 18:47:00 +0545

    Level 4 with bcrypt salting and encryption

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
-
-console.log(md5('123456'));
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -36,32 +33,37 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save(err => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save(err => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
     });
 });
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = (req.body.password);
 
     User.findOne({ email: username }, (err, foundUser) => {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render('secrets');
-                }
+                bcrypt.compare(password, foundUser.password, (error, result) => {
+                    if (result === true) {
+                        res.render('secrets');
+                    }
+                });
             }
         }
     });
